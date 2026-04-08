63300faa3f4e8e219c6d108027ec46cca6bc8e65
Luján <lujiuhalde@hotmail.com>
2026-04-08 20:13:19 -0300

    nuevos cambios

--- src/Program/Board.cs
+++ src/Program/Board.cs
@@ -3,39 +3,52 @@
 namespace Ucu.Poo.GameOfLife
 {
     //Es la clase encargada de realizar el tablero.
-    // En algunos casos podrá llegar a ser el experto de la información.
+    //Tiene la responsabilidad de conocer la posición de las células, además del alto y ancho para hacer la tabla.
     public class Board
     {
-        private bool[,] grid;
-        private int width;
-        private int height;
+        public int Width { get; set; }
+        public int Height { get; set; }
+        private bool[,] cells;
 
-
-    //Recibe de la clase que lee el archivo una matriz en donde se va a obtener el largo y ancho de esta misma.
-        public Board(bool[,] matriz)
+        public Board(bool[,] initialCells)
         {
-            grid = matriz;
-            width = matriz.GetLength(0);
-            height = matriz.GetLength(1);
+            this.Width = initialCells.GetLength(0);
+            this.Height = initialCells.GetLength(1);
+            this.cells = initialCells;
         }
+
+        public bool CellEstate(int x, int y)
+        {
+            return cells[x,y];
+        }
+
 
     //Cell podría ser perfectamente otra clase por SRP.
     //Ya que pueden haber casos en donde se le quiera realizar más de un cambio y, por lo tanto, 
     //tendría que estar en otra clase por separado.
     //Sin embargo, como el ejercicio no solicita nada de ello, decidimos que lo mejor era hacer Cell y Board juntos.
+        public void SetCellState(int x, int y, bool state)
+        {
+            cells[x,y] = state;
+        }
+
+        public void ChangeCellState(int x, int y, bool state)
+        {
+            cells[x,y] = state;
+        }
     
 
     //En esta parte del código se va a verificar que células están vivas.
     // Este metodo va a ser posteriormente llamado por la clase Rules para agregar más especificaciónes 
     // de su estado de vida.
-        private int Cell(int x, int y)
+        public int CellNeighbors(int x, int y)
         {
             int aliveNeighbors = 0;
             for (int i = x - 1; i <= x + 1; i++)
             {
                 for (int j = y - 1; j <= y + 1; j++)
                 {
-                    if (i >= 0 && i < width && j >= 0 && j < height && grid[i, j])
+                    if (i >= 0 && i < Width && j >= 0 && j < Height && cells[i, j])
                     {
                         aliveNeighbors++;
                     }
