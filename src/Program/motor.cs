5f1c06bf806209cb39c3de5e0aedd62689cd49aa
Joaquin Hernandez <67710876+hzjoaco@users.noreply.github.com>
2026-04-11 20:17:49 -0300

    Refactor comments in motor.cs for clarity
    Removed redundant text in design justification comments.

--- src/Program/motor.cs
+++ src/Program/motor.cs
@@ -2,8 +2,7 @@ namespace Ucu.Poo.GameOfLife
 {
     /*
      * JUSTIFICACIÓN DE DISEÑO:
-     * - SRP : La única responsabilidad de esta clase es 
-     * conocer y aplicar las reglas de supervivencia del algoritmo de Conway. Al poder tener más de una 
+     * - SRP : conocer y aplicar las reglas de supervivencia del algoritmo de Conway. Al poder tener más de una 
      * razón de cambio, se realiza la clase motor por separado para poder cumplir con sus responsabilidades correspondientes.
      * - Expert : Es el experto en la lógica del juego. Recibe el 
      * estado actual (Board) y tiene la información del algoritmo para procesar quién vive 
