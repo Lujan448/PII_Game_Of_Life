4bad45e3080e57448bf0e73671cf92ec684ed750
Luján <lujiuhalde@hotmail.com>
2026-04-11 21:11:40 -0300

    cambios en boardimport

--- src/Program/BoardImport.cs
+++ src/Program/BoardImport.cs
@@ -9,16 +9,21 @@ namespace Ucu.Poo.GameOfLife
         public static bool[,] Load(string filePath)
         {
         //lee el archivo y lo pasa a un string para luego conventir esa sting en un arry con el split
-        string ReadFile = File.ReadAllText(filePath);
-        string[] FileLines = ReadFile.Split('\n');
-        //crea la matriz
-        bool [,] board = new bool [FileLines.Length , FileLines[0].Length];
-        // recorre la matriz fijandose si el caracter es 0 o 1 y los remplaza con false o true respectivamente
-        for (int y = 0; y < FileLines.Length ; y++)
+        string readFile = File.ReadAllText(filePath);
+        string[] fileLines = readFile.Split('\n');
+
+        for (int i = 0; i < fileLines.Length; i++)
             {
-                for (int x = 0; x < FileLines[0].Length ; x++)
+                fileLines[i] = fileLines[i].Trim();
+            }
+        //crea la matriz
+        bool [,] board = new bool [fileLines.Length , fileLines[0].Length];
+        // recorre la matriz fijandose si el caracter es 0 o 1 y los remplaza con false o true respectivamente
+        for (int y = 0; y < fileLines.Length ; y++)
+            {
+                for (int x = 0; x < fileLines[0].Length ; x++)
                 {
-                 if (FileLines[y][x]=='1')
+                 if (fileLines[y][x]=='1')
                     {
                         board [y,x]= true;
                     }
